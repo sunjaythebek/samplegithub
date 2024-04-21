48279e2f6c0e216c08809a5eeb75016eb868ec77
Suriya <suntheshah@gmail.com>
2024-04-21 16:37:13 +0530

    adding hashmap.java

---
+++ hashmap.java
@@ -0,0 +1,23 @@
+
+import java.util.HashMap;
+import java.util.*;
+
+public class hashmap {
+
+    public static void main(String[] args) {
+
+        Map <String,Integer> numbers=new HashMap<>();
+
+        numbers.put("One",100);
+        numbers.put("Two",200);
+        numbers.put("Three",300);
+        System.out.println(numbers.entrySet());
+
+
+
+
+        
+
+    }
+    
+}
